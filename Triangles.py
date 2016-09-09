7f827cdd6e50c110257a5c7601abb15ff0c86633
Jmarshall1994 <joshmarshall000@gmail.com>
2016-09-09 11:17:19 -0500

    Add files via upload

---
+++ Triangles.py
@@ -0,0 +1,10 @@
+import math
+
+a = input("Please Enter Side  A ")
+b = input("Please Enter Side  B")
+c = input("Please Enter Side  C ")
+d = a+b+c
+if d < (2*c):
+    print"False"
+elif d > (2*c):
+    print"True "
