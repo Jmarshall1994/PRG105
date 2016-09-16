b338e643c757d72a25ca9199ec6e88968af06025
Jmarshall1994 <joshmarshall000@gmail.com>
2016-09-16 10:10:41 -0500

    Add files via upload

---
+++ 99bottles.py
@@ -0,0 +1,4 @@
+bottles = input('How many bottles?')
+while bottles > 0:
+    print bottles, 'bottles of beer on the wall, ',bottles,' of beer. Take one down and pass it around,', bottles-1,' bottles of beer on the wall'
+    bottles = bottles -1
